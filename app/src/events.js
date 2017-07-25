cb2b6b0536425684de3a1bb2eb06d8a27111f90e
Ivan Reyes <ivanreyesconde@gmail.com>
2017-07-25 14:05:41 -0500

    Se crea evento que limpia el tracklist cuanto termina la reproducción resolves #1

--- app/src/events.js
+++ app/src/events.js
@@ -4,3 +4,5 @@ let backend = require('./backend/backend');
 let mopidy = mpd.mopidy;
 
 mopidy.on("event:trackPlaybackStarted", backend.postSong)
+
+mopidy.on("event:trackPlaybackEnded", backend.clear)
