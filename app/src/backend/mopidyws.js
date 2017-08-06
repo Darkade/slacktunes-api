dea78c9c37851736d150db4b130ce7b295a29d94
Ivan Reyes <ivanreyesconde@gmail.com>
2017-08-06 13:49:13 -0500

    Ready for API Calls message added

--- app/src/backend/mopidyws.js
+++ app/src/backend/mopidyws.js
@@ -1,4 +1,10 @@
 // Instantiates Mopidy Websockets
 var env = require('../env');
 var Mopidy = require('mopidy');
+
+function readyForCalls(){
+  console.log(">>> READY FOR API CALLS <<<");
+}
+
 exports.mopidy = new Mopidy({ webSocketUrl: `ws://${env.mopidyurl}:${env.mopidyport}/mopidy/ws/` });
+exports.mopidy.on("state:online", readyForCalls);
