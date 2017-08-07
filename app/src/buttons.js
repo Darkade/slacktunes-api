292aec7e670bcf64c8ca539267a16761930e3967
Ivan Reyes <ivanreyesconde@gmail.com>
2017-08-07 11:42:02 -0500

    Add functionality moved from buttons to backend

--- app/src/buttons.js
+++ app/src/buttons.js
@@ -1,47 +1,17 @@
 let mpd = require('./backend/mopidyws');
-
 let mopidy = mpd.mopidy;
+let backend = require('./backend/backend');
 
 exports.buttons = function(req, res){
   let body = JSON.parse(req.body.payload);
   let actions = body.actions;
   let callback = body.callback_id;
+  let url = body.response_url;
 
   switch (callback){
     case "addSong":
       let songuri = actions[0].selected_options[0].value;
-
-      mopidy.playback.getState()
-      .then((state)=>{
-        console.log("ESTADO:", state);
-        if (state == "stopped"){
-          console.log("ESTAPARADO")
-          mopidy.tracklist.clear()
-        }
-        return true;
-      })
-      .then(()=>{
-        console.log("Agregar")
-        mopidy.tracklist.add(null, null, songuri, null)
-        .done((tltrack)=>{
-          res.send(`Se agregó ${tltrack[0].track.name}`)
-        })
-        return true;
-      })
-      .then(()=>{
-        mopidy.playback.getState()
-        .then((state)=>{
-          console.log("ESTADO:", state);
-          if (state != "playing"){
-            console.log("ESTAPARADO")
-            mopidy.playback.play()
-          }
-          return true;
-        })
-
-
-      })
-
+      backend.add(songuri, url);
     break;
   }
 }
