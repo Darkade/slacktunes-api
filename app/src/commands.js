e0188025c5d28dd712744974e6b16f55deb334c5
Ivan Reyes <ivanreyesconde@gmail.com>
2017-09-01 17:57:47 -0500

    Added /mopidy https:// command

--- app/src/commands.js
+++ app/src/commands.js
@@ -43,6 +43,32 @@ exports.commands = function(req, res) {
     break;
 
     default:
-      res.send("That's not a slacktunes command. Try typing `/mopidy` to learn more");
+      let re_music = /https?:\/\/play\.google\.com\/music\/m\/(\w+)\??.*/g
+      let re_yt1 = /https?:\/\/youtu\.be\/(\w+)\??.*/g
+      let re_yt2 = /https?:\/\/(www\.)?youtube\.com\/watch\?v=(\w+)&?.*/g
+
+      // Tbzepahs5jrdnaj3rbjuhjin7hi
+      // 6vwNcNOTVzY
+
+      let songid = (re_music.exec(command) || re_yt1.exec(command) || re_yt2.exec(command) || []).pop();
+
+      if (songid){
+        res.send("Let me add that to the playlist...");
+        console.log("<<<<<<", songid, songid.length, songid.length > 11);
+        let uri = "";
+        if (songid.length > 11){
+          console.log("<<<< gmusic");
+          uri = "gmusic:track:" + songid;
+        } else {
+          console.log("<<<< youtube");
+          uri = "yt:http://www.youtube.com/watch?v=" + songid;
+        }
+
+        backend.add(uri, url);
+      }
+      else{
+        res.send("That's not a slacktunes command. Try typing `/mopidy` to learn more");
+      }
+
   }
 }
